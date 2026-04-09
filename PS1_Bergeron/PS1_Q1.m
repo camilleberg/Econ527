5379846df72133a8632c9f5ba1ce12af74ed30e7
Camille Bergeron <camille.bergeron@boston.gov>
2026-04-08 20:11:37 -0700

    added covar

--- PS1_Bergeron/PS1_Q1.m
+++ PS1_Bergeron/PS1_Q1.m
@@ -33,7 +33,7 @@ dtmc_tauchen = dtmc(Zprob_tauchen); % create a discrete-time Markov chain object
 %figure;
 %graphplot(dtmc_tauchen,'ColorEdges',true);
 
-% Simulate the Markov chain
+% Simulate the Markov chain 1000 times 
 parfor i = 1:n_sim
     xo = find(rand < cumsum(invar_dist), 1); % initial state drawn from invariant distribution
     X(:, i) = simulate(dtmc_tauchen, n_duration);
@@ -42,10 +42,15 @@ end
 % figure;
 % simplot(dtmc_tauchen,X)
 
+% checkin mean value 
+disp('Mean value of simulated series:');
+disp(mean(X(:)));
+
+
 % (a) First-order autocorrelation: cor(xt,xt−1)
-x_acf = autocorr(X); % compute autocorrelation function up to lag 1
+x_acf = autocorr(X(:)); % compute autocorrelation function up to lag 1
 disp('First-order autocorrelation:');
-disp(x_acf); % autocorrelation at lag 1
+disp(x_acf(2)); % autocorrelation at lag 1
 
 % (b) Unconditional variance: var(xt)
 % (c) First-order autocovariance: cov(xt,xt−1)
