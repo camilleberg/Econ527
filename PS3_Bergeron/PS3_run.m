56016fccae2348622a6a97a3854792e0e2a1e456
Camille Bergeron <camille.bergeron@boston.gov>
2026-05-06 22:18:08 -0700

    reorgamized

---
+++ PS3_Bergeron/PS3_run.m
@@ -0,0 +1,15 @@
+% Econ 527 Spring 2026 
+% HW 3 
+% Professor Greaney
+% Written by: Camille Bergeron
+% Due: May 08, 2026
+% Last Edited: May 06, 2026
+
+
+%% Questiomn 1: Howard Policy Improvement and Endogenous Grid Method
+run('programs/PS3_Q1_HPI.m'); % run Howard policy improvement code
+
+%% Quesion 1: Endogenous Grid Method
+% run('programs/PS3_Q1_EGM.m'); % run Endogenous Grid Method code
+
+%% Question 2: Stationary Distribution and Income Fluctuations
