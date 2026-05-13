3963d8213ec926c36805639903cd62c51815a072
Camille Bergeron <camille.bergeron@boston.gov>
2026-05-12 22:28:34 -0700

    started

---
+++ PS4_Bergeron/f_nad.m
@@ -0,0 +1,3 @@
+function [net_assets] = f_spe(r, markov_chain, u_fxn, u_prime_inv, Params, a_grid, a_fine_grid)
+    [v_fxn, policy_fxn, phi_dist, net_assets] = f_spe(r, markov_chain, u_fxn, u_prime_inv, Params, a_grid, a_fine_grid);
+
